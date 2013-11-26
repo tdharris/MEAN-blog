648952948c549b5a45134b864a2e2920056559d7
tdh <tdh@ubu.(none)>
2013-11-26 00:07:22 -0700

    added AngularJS with bower

--- app.js
+++ app.js
@@ -34,3 +34,6 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var Mongoose = require('mongoose');
+var db = Mongoose.createConnection('localhost', 'mytestapp');
