f445d75787957a60d66e3edeb5096f417bed6507
tdh <tdh@ubu.(none)>
2013-12-03 00:10:57 -0700

    working pretty good..

--- app.js
+++ app.js
@@ -1,39 +1,112 @@
-
 /**
  * Module dependencies.
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+var ArticleProvider = require('./articleprovider-mongodb').ArticleProvider;
 
-var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var app = module.exports = express.createServer();
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+// Configuration
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-var Mongoose = require('mongoose');
-var db = Mongoose.createConnection('localhost', 'mytestapp');
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+var articleProvider = new ArticleProvider('localhost', 27017);
+// Routes
+
+app.get('/', function(req, res){
+    articleProvider.findAll( function(error,docs){
+        res.render('index', { 
+            locals: {
+                title: 'a simple blog',
+                articles:docs
+            }
+        });
+    })
+});
+
+app.get('/blog/new', function(req, res) {
+    res.render('blog_new', { locals: {
+        title: 'new post'
+    }
+    });
+});
+
+app.post('/blog/new', function(req, res){
+    articleProvider.save({
+        title: req.param('title'),
+        body: req.param('body')
+    }, function( error, docs) {
+        res.redirect('/')
+    });
+});
+
+app.get('/blog/:id', function(req, res) {
+    articleProvider.findById(req.params.id, function(error, article) {
+        res.render('blog_show',
+        { locals: {
+            title: article.title,
+            article: article
+        }
+        });
+    });
+});
+
+// edit a blog post
+app.get('/blog/:id/edit', function(req, res) {
+	articleProvider.findById(req.param('_id'), function(error, article) {
+		res.render('blog_edit',
+		{ locals: {
+			title: article.title,
+			article: article
+		}
+		});
+	});
+});
+
+// update/save an edited blog post
+app.post('/blog/:id/save', function(req, res) {
+	articleProvider.update(req.param('_id'),{
+		title: req.param('title'),
+		body: req.param('body')
+	}, function(error, docs) {
+		res.redirect('/')
+	});
+});
+
+// delete a blog post
+app.post('/blog/:id/delete', function(req, res) {
+		articleProvider.delete(req.param('_id'), function(error, docs) {
+			res.redirect('/')
+		});
+});
+
+app.post('/blog/addComment', function(req, res) {
+    articleProvider.addCommentToArticle(req.param('_id'), {
+        person: req.param('person'),
+        comment: req.param('comment'),
+        created_at: new Date()
+       } , function( error, docs) {
+           res.redirect('/blog/' + req.param('_id'))
+       });
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
